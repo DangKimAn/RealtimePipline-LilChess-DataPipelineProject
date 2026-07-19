6d68623045e4e768c621b5959ae02d503ee904e5
DangKimAn <dangkiman41005@gmail.com>
2026-07-19 11:24:38 +0700

    fix : add more variable

--- include/script/spark/spark.py
+++ include/script/spark/spark.py
@@ -1,11 +1,15 @@
 from pyspark.sql import SparkSession
 
 
-spark = SparkSession.builder \
-    .appName("ChessKafkaConsumer") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.2.0") \
-    .master("local[*]") \
-    .getOrCreate()
+# spark = SparkSession.builder \
+#     .appName("ChessKafkaConsumer") \
+#     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.2.0") \
+#     .master("local[*]") \
+#     .getOrCreate()
 
+spark = SparkSession.builder \
+    .appName("ChessRealTime") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.2.0,org.postgresql:postgresql:42.7.3") \
+    .getOrCreate()
 # Tắt bớt log rác của Spark để màn hình Console dễ nhìn hơn
 spark.sparkContext.setLogLevel("WARN")
