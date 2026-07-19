734e043074fd2dd2bc5841b6242552a4dfaee6e5
DangKimAn <dangkiman41005@gmail.com>
2026-07-20 00:29:10 +0700

    fix : add enviroment

--- include/script/spark/spark.py
+++ include/script/spark/spark.py
@@ -1,15 +1,18 @@
+import os
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+env_path_file = os.path.join('/'.join(script_dir.split('/')[:-3]), '.env')
+load_dotenv(env_path_file)
 
-# spark = SparkSession.builder \
-#     .appName("ChessKafkaConsumer") \
-#     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.2.0") \
-#     .master("local[*]") \
-#     .getOrCreate()
+SCALA_VERSION = os.getenv('SCALA_VERSION')
+SPARK_KAFKA_VERSION = os.getenv('SPARK_KAFKA_VERSION')
+POSTGRES_JDBC_VERSION = os.getenv('POSTGRES_JDBC_VERSION')
 
 spark = SparkSession.builder \
     .appName("ChessRealTime") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.2.0,org.postgresql:postgresql:42.7.3") \
+    .config("spark.jars.packages", f"org.apache.spark:spark-sql-kafka-0-10_{SCALA_VERSION}:{SPARK_KAFKA_VERSION},org.postgresql:postgresql:{POSTGRES_JDBC_VERSION}") \
     .getOrCreate()
 # Tắt bớt log rác của Spark để màn hình Console dễ nhìn hơn
 spark.sparkContext.setLogLevel("WARN")
